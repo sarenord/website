cf1bc75ea4aee898486f33ed090a6dfb39b45b50
sarenord <ajsheld12@gmail.com>
2020-03-26 14:31:33 -0400

    added skeletal structure for pages

--- pages.go
+++ pages.go
@@ -1,64 +1,95 @@
 package main
 
-import(
-	"os"
-	"net/http"
-	"html/template"
+import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"io/ioutil"
+	"net/http"
 )
 
 // This is basically the page structure configuration for the whole site
 
 type PageBlueprint struct {
 	Template string
-	Label string
-	Styles []string
-	Scripts []string
+	Label    string
+	Styles   []string
+	Scripts  []string
 }
 
-type ServablePage{
-	Body html.Template
+type ServablePage struct {
+	Body    []byte
 	Scripts []string
-	Styles []string
+	Styles  []string
 }
 
 var pages = map[string]PageBlueprint{
 	"home": PageBlueprint{
 		Template: "landing.html",
-		Label: "homepage.html",
-		Styles: []string{ "landing.css", "homepage.css" },
-		Scripts: []string{ "landing.js", "homepage.js" },
+		Label:    "homepage.html",
+		Styles:   []string{"landing.css", "homepage.css"},
+		Scripts:  []string{"landing.js", "homepage.js"},
 	},
 	"about": PageBlueprint{
 		Template: "landing.html",
-		Label: "about.html",
-		Styles: []string{ "landing.css", "about.css" },
-		Scripts: []string{ "landing.js", "about.js" },
+		Label:    "about.html",
+		Styles:   []string{"landing.css", "about.css"},
+		Scripts:  []string{"landing.js", "about.js"},
 	},
 	"projects": PageBlueprint{
 		Template: "landing.html",
-		Label: "projects.html",
-		Styles: []string{ "landing.css", "projects.css" },
-		Scripts: []string{ "landing.js", "projects.js" },
+		Label:    "projects.html",
+		Styles:   []string{"landing.css", "projects.css"},
+		Scripts:  []string{"landing.js", "projects.js"},
 	},
 	"contact": PageBlueprint{
 		Template: "landing.html",
-		Label: "contact.html",
-		Styles: []string{ "landing.css", "contact.css" },
-		Scripts: []string{ "landing.js", "contact.js" },
+		Label:    "contact.html",
+		Styles:   []string{"landing.css", "contact.css"},
+		Scripts:  []string{"landing.js", "contact.js"},
 	},
 }
 
-func (p *PageBlueprint)assemble(w http.ResponseWriter) template.HTML {
-	baseTemplate := template.Must(template.New("base").ParseFiles("./resources/templates/"+p.Template));
-	body := template.Must(template.New().ParseFiles("./resources/pages/"+p.Label));
-	scripts := [len(p.Scripts)]string;
-	for (int i=0; i<len(scripts); i++) {
-		scripts[i] = ioutil.ReadFile(
-	}
-	fmt.Printf("%s", baseTemplate);
+func (p *PageBlueprint) assemble(w http.ResponseWriter) {
+	var err error
+	var body []byte
+	scripts := make([]string, len(p.Scripts))
+	styles := make([]string, len(p.Styles))
 
-	var r template.HTML;
-	return r;
+	body, err = ioutil.ReadFile("./resources/pages/" + p.Label)
+	fmt.Printf("Template: %s", p.Template);
+	baseTemplate := template.Must(template.New("base").ParseFiles("./resources/templates/" + p.Template))
+
+	for i := 0; i < len(p.Scripts); i++ {
+		var script []byte
+		script, err = ioutil.ReadFile("./resources/scripts/" + p.Scripts[i])
+		scripts[i] = string(script)
+	}
+	for i := 0; i < len(p.Styles); i++ {
+		var style []byte
+		style, err = ioutil.ReadFile("./resources/styles/" + p.Styles[i])
+		styles[i] = string(style)
+	}
+
+	if err != nil {
+		panic(err)
+		fmt.Printf("error assembling page")
+	}
+
+	var r ServablePage = ServablePage{
+		Body:    body,
+		Styles:  styles,
+		Scripts: scripts,
+	}
+
+	templateStager := new(bytes.Buffer)
+	baseTemplate.Execute(templateStager, r)
+	l2Template := template.Must(template.New("final").Parse(templateStager.String()))
+	l2Template.Execute(w, r)
+	fmt.Printf("%s", baseTemplate)
+}
+
+func fromName(l string) PageBlueprint {
+	fmt.Printf("%s", pages[l]);
+	return pages[l]
 }
--- server.go
+++ server.go
@@ -1,46 +1,18 @@
 package main
 
 import (
-	"net/http"
-	"fmt"
-	"io/ioutil"
 	"log"
-	"html/templates"
+	"net/http"
+	"strings"
 )
 
-struct Page {
-	Title string
-	Body []byte
-}
-
 func main() {
-	http.HandleFunc("/", pageRouter);
-	log.Fatal(http.ListenAndServe(":8080", nil));
+	http.HandleFunc("/", pageRouter)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func pageRouter(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path[1] {
-	case "homepage":
-
-	case "app":
-		loadApplication(r.URL.Path[len("/app/"):]);
-	default:
-		http.Redirect(w, r, "/homepage", 301);
-	}
-}
-
-func loadMainPage() (*Page, error) {
-	html, err := ioutil.ReadFile("./homepage.html");
-	if err != nil {
-		return nil, err;
-	}
-	return &Page{Title: title, Body: html}, nil;
-}
-
-func loadPage(title string) *Page {
-
-}
-
-func loadApplication(title string) {
-
+	resourcePath := strings.Split(r.URL.Path, "/")
+	var p PageBlueprint = fromName(resourcePath[len(resourcePath)-1])
+	p.assemble(w);
 }
